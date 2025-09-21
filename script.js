7b03e5c617faae4ea01dd7565f8492f405876fc8
daniele2076 <157383717+daniele2076@users.noreply.github.com>
2025-09-21 13:55:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,10 @@
 const files = [
   { name: 'Hallowen 1', file: 'pages/Hallowen 1.html' },
   { name: 'Hallowen 2', file: 'pages/Hallowen 2.html' },
-  { name: 'Scienza', file: 'scienza.html' }
+  { name: 'School Subjects 1', file: 'pages/materie 1.html' }
+  { name: 'Animals 1', file: 'pages/inglese1.html' }
 ];
+
 
 
 
