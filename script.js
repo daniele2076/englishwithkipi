fe499f686f58cd4d9837eca1e8085317e2bf8a5d
daniele2076 <157383717+daniele2076@users.noreply.github.com>
2025-09-21 13:57:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const files = [
   { name: 'Hallowen 1', file: 'pages/Hallowen 1.html' },
   { name: 'Hallowen 2', file: 'pages/Hallowen 2.html' },
-  { name: 'School Subjects 1', file: 'pages/materie 1.html' }
+  { name: 'School Subjects 1', file: 'pages/materie 1.html' },
   { name: 'Animals 1', file: 'pages/inglese1.html' }
 ];
 
@@ -20,6 +20,7 @@ files.forEach(item => {
   };
   linkList.appendChild(link);
 });
+
 
 
 
